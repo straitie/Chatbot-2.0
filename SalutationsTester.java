eea7629f55017c1b3f50609400bdaa6301338650
straitie <113708643+straitie@users.noreply.github.com>
2022-10-07 08:26:20 -0700

    Add files via upload

---
+++ SalutationsTester.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class SalutationsTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SalutationsTester
+{
+    public static void main(String[] args)
+    {
+        Salutations sal1 = new Salutations("Eden");
+        sal1.addressLetter();
+        sal1.signLetter();
+        sal1.addressMemo();
+        sal1.signMemo();
+    }
+}
